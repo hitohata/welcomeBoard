76082d73af685b09b5c36db93621bf15d558b36c
hirohata <hirohatatro@gmail.com>
2022-08-02 19:01:55 +0900

    add api gateway

--- lib/welcome-board-stack.ts
+++ lib/welcome-board-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambdaGo from "@aws-cdk/aws-lambda-go-alpha";
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
 import * as path from "path";
 
 interface IProps extends StackProps {
@@ -29,5 +30,13 @@ export class WelcomeBoardStack extends Stack {
     );
 
     messageTable.grantFullAccess(messageFunction);
+
+    const api = new apiGateway.RestApi(this, "apiGateway", {});
+    const resource = api.root.addResource("v1");
+
+    resource.addProxy({
+      defaultIntegration: new apiGateway.LambdaIntegration(messageFunction),
+      anyMethod: true
+    })
   }
 }
