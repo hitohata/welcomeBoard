933aad4a2f9d857f2f9e7abd94c7843667d30fa2
hirohata <hirohatatro@gmail.com>
2022-08-13 19:12:39 +0900

    add a create dynamo test table container

--- lambda/welcomeBoard/test_db_schema/main.go
+++ lambda/welcomeBoard/test_db_schema/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"log"
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -12,16 +12,18 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-type Message struct {
-	KeyWord string `dynamodbav: "keyword"`
-	Name string `dynamodbav: "name"`
-	Message string `dynamodbav: "message"`
-}
-
 const AWS_REGION = "ap-northeast-1"
-const DYNAMO_ENDPOINT = "http://welcome-message-local"
+const DYNAMO_ENDPOINT = "http://welcome-message-dynamo:8000"
 
 func main() {
+
+	custom_resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			PartitionID:   "aws",
+			URL:           DYNAMO_ENDPOINT,
+			SigningRegion: AWS_REGION,
+		}, nil
+	})
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion("ap-northeast-1"),
@@ -30,10 +32,12 @@ func main() {
 				AccessKeyID:     "dummyKey",
 				SecretAccessKey: "dummyKey",
 			},
-		}))
+		}),
+		config.WithEndpointResolverWithOptions(custom_resolver),
+	)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("InvalidConfig", err)
 	}
 
 	client := dynamodb.NewFromConfig(cfg)
@@ -42,25 +46,27 @@ func main() {
 		AttributeDefinitions: []types.AttributeDefinition{
 			{
 				AttributeName: aws.String("Keyword"),
-				AttributeType: "S",
+				AttributeType: types.ScalarAttributeTypeS,
 			},
 		},
 		KeySchema: []types.KeySchemaElement{
 			{
 				AttributeName: aws.String("Keyword"),
-				KeyType: "HASH",
+				KeyType:       types.KeyTypeHash,
 			},
 		},
-		TableName: aws.String("WeddingMessage"),
-		BillingMode: "PAY_PAR_REQUEST",
+		TableName:   aws.String("WeddingMessage"),
+		BillingMode: types.BillingModePayPerRequest,
 	}
 
 	resp, err := client.CreateTable(context.TODO(), create_table_input)
 
-	if (err != nil) {
-		log.Fatal(err.Error())
+	if err != nil {
+		log.Fatal("CreateTableFailed:", err.Error())
 	}
 
-	fmt.Println("resp", resp)
-
+	fmt.Println(*resp.TableDescription.TableArn)
+	fmt.Println(resp.TableDescription.KeySchema)
+	fmt.Println(*resp.TableDescription.TableName)
+	fmt.Println(resp.TableDescription.AttributeDefinitions)
 }
